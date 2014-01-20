31569ff3685002cda907ff4df1ccc84fc03a7cfb
larry <libin2722@sohu.com>
2014-01-20 16:49:21 +0800

    add identity

--- fw-core/src/main/java/org/larry/fw/common/entity/AbstractEntity.java
+++ fw-core/src/main/java/org/larry/fw/common/entity/AbstractEntity.java
@@ -2,6 +2,23 @@ package org.larry.fw.common.entity;
 
 import java.io.Serializable;
 
-public class AbstractEntity<ID extends Serializable> {
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.springframework.data.domain.Persistable;
 
+public abstract class AbstractEntity<ID extends Serializable> implements Persistable<ID>, Serializable {
+
+	public abstract ID getId();
+	
+	public abstract void setId(final ID id);
+	
+	@Override
+	public boolean isNew() {
+		return null == getId();
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
+	}
+	
 }
