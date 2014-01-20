d3dea66e84ce9c2723b2aa8edb84783b87dfc428
larry <libin2722@sohu.com>
2014-01-20 16:50:12 +0800

    add identity

---
+++ fw-core/src/main/java/org/larry/fw/common/entity/IdEntity.java
@@ -0,0 +1,39 @@
+package org.larry.fw.common.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
+import javax.persistence.PreUpdate;
+
+@MappedSuperclass
+public abstract class IdEntity<ID extends Serializable> extends AbstractEntity<ID> {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private ID id;
+
+	@Override
+	public ID getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(ID id) {
+		this.id = id;
+	}
+
+	@PrePersist
+	protected abstract void prePersist();
+
+	@PreUpdate
+	protected abstract void preUpdate();
+
+	@PreRemove
+	protected abstract void preRemove();
+
+}
